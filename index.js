f0f4ccb963bc4edbaa411e4a178a5e34c20be868
Jack Fraley <fraleyj@oregonstate.edu>
2023-11-29 16:00:07 -0800

    comments added

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
+//required files to pass data to openai and to server
 const express = require("express")
 const cors = require('cors');
 require("dotenv").config()
 const OpenAI = require('openai').default;
 
+//get openai key from .env file
+//leave .env in .gitignore, DO NOT PUSH TO GITHUB
 const openai = new OpenAI({
     apiKey: process.env.OPEN_AI_KEY
   });
 
 const app = express()
-
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json())
 
+//get todays date
 let currentDate =new Date();
 let formattedDate = `${currentDate.getMonth() +1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
-//let dayofTheWeek = `${currentDate.getDay()}`
 
+//post request to server
 app.post('/calendar', async (req, res) => {
     const userInput = req.body.text;
-    // Process with GPT
+    //get gpt response
     const gptResponse = await openai.chat.completions.create({
         model: "gpt-4",
         messages: [{
@@ -27,12 +30,14 @@ app.post('/calendar', async (req, res) => {
             content: `Extract the event details from the user's input. Begin by identifying the action the user wants to perform from the list: "add, delete, change", then get the events name. Then, determine the event's date based on today's date which is ${formattedDate}, formatted as "month/day/year". If the user wants this event to be repeated, add the additional dates underneath the eventDate. If no event time is specified, default to "All Day". Finally present the information as follows with the value of each item inside parentheses:
 
             Desired Format:
-            eventaction: ()
+            eventAction: ()
             eventName: ()
             eventDate: ()
-            eventTime: ()
-            User Input: ${userInput}`,
+            eventTime: ()`
         }],
+        //gpt response variables
+        //max_tokens changes complexity of response, higher costs more money
+        //temperature changes randomness of answer
         max_tokens: 500,
         temperature: 0.2,
         top_p: 1.0,
@@ -41,12 +46,13 @@ app.post('/calendar', async (req, res) => {
     }); 
     
 
-    console.log(formattedDate)
+    //console.log(formattedDate)
     
     
     const chatMessage  = gptResponse.choices[0].message.content;
-    //gptResponse;
-    console.log(chatMessage)
+    //output the gpt response
+    //pull the specific data from the response
+    //console.log(chatMessage)
     var regExp = /\(([^)]+)\)/;
     var matches = regExp.exec(chatMessage);
     var eventAction = matches[1];
@@ -66,15 +72,13 @@ app.post('/calendar', async (req, res) => {
     }*/
 
    // res.json({ action: action, content: content });
-
+//pass the response back to the server
 res.json({content: eventAction, content: eventDate, content: eventTime, content: eventName})
 });
 
-
+//information needed to run the server
 const port = process.env.PORT || 3000;
-
 const server = app.listen(port, () => console.log(`Server listening on port ${port}`));
-
 process.on('SIGINT', () => {
     console.log('Process terminated. Shutting down server...');
     server.close(() => {
